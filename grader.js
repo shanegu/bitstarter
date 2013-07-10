2678b5fad3c30587f1f7eb87219d01710866ef51
shanegu <5ujiedai@gmail.com>
2013-07-10 05:36:47 +0000

    final submitted grader.js with option --url or --file

--- grader.js
+++ grader.js
@@ -22,39 +22,49 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://enigmatic-cliffs-6555.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
-    return out;
+//    return out;
+    console.log(JSON.stringify(out, null, 4));
 };
 
+var readHtmlFile = function(htmlfile,checksfile) {
+    var $ = cheerio.load(fs.readFileSync(htmlfile));
+    checkHtmlFile($, checksfile);
+};
+
+var readurl = function(url,checksfile) {
+    rest.get(url).on('complete', function(result) {
+	var $ = cheerio.load(result);
+	checkHtmlFile($, checksfile);
+    });
+};
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -63,12 +73,20 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('-u, --url <url_address>', 'Path to .html from the web', URL_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.parse(process.argv);
+    if(program.url) {
+//       var checkJson = readurl(program.url, program.checks);
+       readurl(program.url, program.checks);	
+    } else {
+//       var checkJson = readHtmlFile(program.file, program.checks);
+       readHtmlFile(program.file, program.checks);
+    }
+//    var outJson = JSON.stringify(checkJson, null, 4);
+//    console.log(outJson);
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.readHtmlFile = readHtmlFile;
+    exports.readurl = readurl;
 }
